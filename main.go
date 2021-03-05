e47255e38fefd4766daa1bad74174b0929201929
Toni Le <toni.le@outlook.de>
2021-03-05 21:38:42 +0100

    Memory usage

--- main.go
+++ main.go
@@ -7,10 +7,12 @@ import (
 	"time"
 
 	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/mem"
 )
 
 func main() {
 	fmt.Println(getCpuUsage())
+	fmt.Println(getMemoryUsage())
 }
 
 func getCpuUsage() int {
@@ -20,4 +22,13 @@ func getCpuUsage() int {
 	}
 
 	return int(math.Ceil(percent[0]))
+}
+
+func getMemoryUsage() int {
+	memory, err := mem.VirtualMemory()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return int(math.Ceil(memory.UsedPercent))
 }
